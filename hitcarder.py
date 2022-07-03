afc71acd477b8987a362d23f2c17660ec0e99069
kyle <54095225+Mountain-D-Kyle@users.noreply.github.com>
2022-07-03 10:03:28 +0800

    Update hitcarder.py

--- hitcarder.py
+++ hitcarder.py
@@ -135,8 +135,11 @@ class HitCarder(object):
         new_info['address'] = old_info['address']
         new_info['area'] = old_info['area']
         new_info['city'] = old_info['city']
+        new_info['sqhzjkkys'] = 1 #健康码：绿码
+        new_info['tw'] = 0 # #发热：无
         new_info['internship'] =1 #实践：不实习
-        new_info['ismoved'] = 0
+        new_info['sfcxzysx'] =0 # 涉疫情的管控措施：无
+        new_info['sfjcbh'] =0 #接触的情况：无
         new_info['sfzx'] = old_info['sfzx'] # 在校
         new_info['sfymqjczrj'] = old_info['sfymqjczrj'] # 入境
         new_info['sfqrxxss'] = 1 # 属实
